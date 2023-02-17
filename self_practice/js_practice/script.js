fee8dd0f5135a71265e144c98cfd9b74a4b090f4
”ChenBingWei1201” <rain4030633@gmail.com>
2023-02-17 17:40:31 +0800

    modified

--- self_practice/js_practice/script.js
+++ self_practice/js_practice/script.js
@@ -1,11 +1,1 @@
-let header = "Templates Literals";
-let tags = ["template literals", "javascript", "es6"];
-
-let html = `<h2>${header}</h2><ul>`;
-
-for (const x of tags) {
-  html += `<li>${x}</li>`;
-}
-
-html += `</ul>`;
-document.getElementById("demo").innerHTML = html;
+document.getElementById("demo").innerHTML = document.getElementsById("1").nodeType;
