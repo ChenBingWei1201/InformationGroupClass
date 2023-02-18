ef80aeb258ffdb1326a6e4efab357754ab12c6df
”ChenBingWei1201” <rain4030633@gmail.com>
2023-02-18 17:13:20 +0800

    practice react

---
+++ self_practice/my-app/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react'; // entry files
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App'; // 從App.js import App()
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root')); // react 內唯一會用到getElementById
+root.render( // 渲染
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
