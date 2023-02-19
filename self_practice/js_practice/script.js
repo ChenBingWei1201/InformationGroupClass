8ef57be5582291bd0e10039a526e64b309a7e618
”ChenBingWei1201” <rain4030633@gmail.com>
2023-02-19 21:20:08 +0800

    modified

--- self_practice/js_practice/script.js
+++ self_practice/js_practice/script.js
@@ -1,1 +1,14 @@
-document.getElementById("demo").innerHTML = document.getElementsById("1").nodeType;
+const myVehicle = {
+    brand: 'Ford',
+    model: 'Mustang',
+    color: 'red'
+}
+
+const updateMyVehicle = {
+    type: 'car',
+    year: 2021,
+    color: 'yellow'
+}
+
+const myUpdatedVehicle = { ...myVehicle, ...updateMyVehicle }
+console.log(myUpdatedVehicle);
