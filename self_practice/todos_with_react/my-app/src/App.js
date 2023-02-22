3d41d8b40226dfd9398f5217e5267d164412bb07
”ChenBingWei1201” <rain4030633@gmail.com>
2023-02-22 16:37:27 +0800

    modified: App.js

--- self_practice/todos_with_react/my-app/src/App.js
+++ self_practice/todos_with_react/my-app/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-
+import X from './images/x.png'
 function App() {
   return (
     <div id="root" className="todo-app__root">
@@ -11,11 +11,16 @@ function App() {
       <section className="todo-app__main">
         <input id="todo-input" className="todo-app__input" placeholder="What needs to be done?" />
         <ul id="todo-list" className="todo-app__list">
-
+          <li className="todo-app__item" id="l1-0"> {/* 當按下enter, new一個新的item*/}
+            <div className="todo-app__checkbox">
+              <input id="0" type="checkbox" />
+              <label htmlFor="0"></label>
+            </div>
+            <h1 className="todo-app__item-detail">This is the first item.</h1>
+            <img className="todo-app__item-x" src={X} alt="x" />
+          </li>
         </ul>
-
       </section>
-
       <footer className="todo-app__footer" id="todo-footer">
         <div className="todo-app__total">
           <span id="todo-app-count">0</span> left
@@ -29,8 +34,6 @@ function App() {
           <button id="todo-clear-completed">Clear completed</button>
         </div>
       </footer>
-
-      <script src="scripts/practice.js"></script>
     </div>
   );
 }
