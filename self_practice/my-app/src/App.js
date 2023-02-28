1201bb05d98a35824f992ff35a3f793c811c3cd7
”ChenBingWei1201” <rain4030633@gmail.com>
2023-02-28 21:13:23 +0800

    modified

--- self_practice/my-app/src/App.js
+++ self_practice/my-app/src/App.js
@@ -1,39 +1,13 @@
-// import logo from './logo.svg';
-import './App.css';
 import React from 'react';
-import { useState } from "react";
-// function App() {
-//   return ( // JSX syntax:
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+import './App.css';
+import TodoList from './components/TodoList';
 
 function App() {
-  const [count, setCount] = useState(100);
   return (
-    <div className="App">
-      <h1 className='App-display'>{count}</h1>
-      <span className='App-controls'>
-        <button className='Plus' onClick={() => setCount((c) => c + 1)}>+</button>
-        <button className='Minus' onClick={() => setCount((c) => c - 1)}>-</button>
-      </span>
+    <div className='todo-app'>
+      <TodoList />
     </div>
-  )
+  );
 }
 
-export default App; // export App()
+export default App;
--- self_practice/my-app/src/index.js
+++ self_practice/my-app/src/index.js
@@ -1,19 +1,10 @@
-import React from 'react'; // entry files
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App'; // 從App.js import App()
-import reportWebVitals from './reportWebVitals';
-// import Car2 from './Car';
-// import Car_f from './Car_f';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root')); // react 內唯一會用到getElementById
-root.render( // 渲染
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
