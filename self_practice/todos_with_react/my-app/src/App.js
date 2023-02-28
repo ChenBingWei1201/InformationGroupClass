687a30a6c7341e5d6d474dd96d1fa97abc36adbd
”ChenBingWei1201” <rain4030633@gmail.com>
2023-02-28 21:15:42 +0800

    modified App.js and index.js

--- self_practice/todos_with_react/my-app/src/App.js
+++ self_practice/todos_with_react/my-app/src/App.js
@@ -1,41 +1,24 @@
 import './App.css';
-import X from './images/x.png'
-function App() {
+import TodoMain from './TodoMain';
+import Header from './Header';
+// import { useState } from 'react';
+function Todo() {
+  // const [task, setTask] = useState("");
+  // const [value, setValue] = useState("");
+  // const [keyboard, setKeyboard] = useState("");
+  // const [id, setId] = useState(0);
+  // const [todoData, setData] = useState([]);
   return (
     <div id="root" className="todo-app__root">
-
-      <header className="todo-app__header">
-        <h1 className="todo-app__title">todos</h1>
-      </header>
-
-      <section className="todo-app__main">
-        <input id="todo-input" className="todo-app__input" placeholder="What needs to be done?" />
-        <ul id="todo-list" className="todo-app__list">
-          <li className="todo-app__item" id="l1-0"> {/* 當按下enter, new一個新的item*/}
-            <div className="todo-app__checkbox">
-              <input id="0" type="checkbox" />
-              <label htmlFor="0"></label>
-            </div>
-            <h1 className="todo-app__item-detail">This is the first item.</h1>
-            <img className="todo-app__item-x" src={X} alt="x" />
-          </li>
-        </ul>
-      </section>
-      <footer className="todo-app__footer" id="todo-footer">
-        <div className="todo-app__total">
-          <span id="todo-app-count">0</span> left
-        </div>
-        <ul className="todo-app__view-buttons">
-          <li><button id="todo-all">All</button></li>
-          <li><button id="todo-active">Active</button></li>
-          <li><button id="todo-completed">Completed</button></li>
-        </ul>
-        <div className="todo-app__clean">
-          <button id="todo-clear-completed">Clear completed</button>
-        </div>
-      </footer>
+      <Header />
+      <TodoMain />
     </div>
   );
 }
 
-export default App;
+export default Todo;
+// task={task} setTask={setTask}
+//         value={value} setValue={setValue}
+//         keyboard={keyboard} setKeyboard={setKeyboard}
+//         id={id} setId={setId}
+//         todoData={todoData} setData={setData} 
