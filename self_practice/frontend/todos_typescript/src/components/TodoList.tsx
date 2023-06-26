f03e26e62c5db1ff3b884516cefa4295fe34b825
ChenBingWei1201 <rain4030633@gmail.com>
2023-06-26 22:19:09 +0800

    todos practice add list

--- self_practice/frontend/todos_typescript/src/components/TodoList.tsx
+++ self_practice/frontend/todos_typescript/src/components/TodoList.tsx
@@ -1,16 +1,49 @@
-function TodoList(): JSX.Element {
+import X from '../images/x.png';
+
+interface DATA {
+    data: any;
+    setData: React.Dispatch<any>;
+    id: number;
+    setID: React.Dispatch<React.SetStateAction<number>>;
+};
+
+type Item = {
+    task: string,
+    id: number,
+    checked: boolean
+};
+
+type KeyboardEventNames = 'keydown' | 'keyup';
+
+function TodoList({data, setData, id, setID}: DATA): JSX.Element {
+    const listenInput = (e: KeyboardEvent): void => {
+        if (e.key !== "Enter") {
+            return;
+        }
+        const task: string = (e.target as HTMLInputElement).value;
+        const item: Item =  {task, id, checked: false};
+        addTodoItem(item);
+    }
+
+    const addTodoItem = (item: Item): JSX.Element => {
+
+        return (
+            <li className="todo-app__item" id= {`li-0`}>  
+                <div className="todo-app__checkbox">
+                    <input id={`0`} type="checkbox" />
+                    <label htmlFor={`0`}></label>
+                </div>
+                <h1 className="todo-app__item-detail">This is the first TODO.</h1>
+                <img className="todo-app__item-x" src={X} alt="x" />
+            </li> 
+        );
+    }
+
     return (
         <section className="todo-app__main">
-            <input id="todo-input" className="todo-app__input" placeholder="What needs to be done?" />
+            <input id="todo-input" className="todo-app__input" placeholder="What needs to be done?" onKeyDown={listenInput}/>
             <ul id="todo-list" className="todo-app__list">
-                <li className="todo-app__item" id="li-0">  
-                        <div className="todo-app__checkbox">
-                            <input id="0" type="checkbox" />
-                            <label htmlFor="0"></label>
-                        </div>
-                        <h1 className="todo-app__item-detail">This is the first TODO.</h1>
-                        <img className="todo-app__item-x" src="./images/x.png" alt="x" />
-                </li> 
+                {/* {todoItem()} */}
             </ul>
 
         </section>
--- self_practice/frontend/todos_typescript/src/components/TodoMain.tsx
+++ self_practice/frontend/todos_typescript/src/components/TodoMain.tsx
@@ -1,9 +1,17 @@
+import { useState } from 'react';
 import TodoFoot from './TodoFoot';
 import TodoList from './TodoList';
+
+
 function TodoMain(): JSX.Element {
+    
+    const [data, setData] = useState<any>({});
+    const [id, setID] = useState<number>(0);
+    const [status, setStatus] = useState<string>("All");
+    
     return (
         <div>
-            <TodoList />
+            <TodoList data={data} setData={setData} id={id} setID={setID}/>
             <TodoFoot />
         </div>
     );
