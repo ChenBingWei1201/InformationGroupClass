31731a840c60dc7da91b73fc96b76fed64c2c80b
ChenBingWei1201 <rain4030633@gmail.com>
2023-07-04 17:05:03 +0800

    create user

--- self_practice/backend/hello-prisma/index.ts
+++ self_practice/backend/hello-prisma/index.ts
@@ -2,28 +2,13 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-async function main() {
+const main = async () => {
   // ... you will write your Prisma Client queries here
-  await prisma.user.create({
-    data: {
-      name: 'Alice',
-      email: 'alice@prisma.io',
-      posts: {
-        create: { title: 'Hello World' },
-      },
-      profile: {
-        create: { bio: 'I like turtles' },
-      },
-    },
+  const post = await prisma.post.update({
+    where: { id: 2 },
+    data: { content: 'Hi!' },
   })
-
-  const allUsers = await prisma.user.findMany({
-    include: {
-      posts: true,
-      profile: true,
-    },
-  })
-  console.dir(allUsers, { depth: null })
+  console.log(post);
 }
 
 main()
